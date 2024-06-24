85d6a7f6e52166e9f73b4ae4bf236bae4e374c8c
Lenqo <lenqo321@gmail.com>
2024-06-24 21:19:01 +0300

    Feat: add a graphical interface, set program name

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,24 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QPixmap pix{":img/img/pointer.png"};
+    pix = pix.scaled(50, 50, Qt::KeepAspectRatio);
+    ui->pointer->setPixmap(pix);
+}
+
+void MainWindow::start() {
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_MainWindow_iconSizeChanged(const QSize &iconSize)
+{
+
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPixmap>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -13,7 +14,13 @@ class MainWindow : public QMainWindow
 
 public:
     MainWindow(QWidget *parent = nullptr);
+
+    void start();
+
     ~MainWindow();
+
+private slots:
+    void on_MainWindow_iconSizeChanged(const QSize &iconSize);
 
 private:
     Ui::MainWindow *ui;
